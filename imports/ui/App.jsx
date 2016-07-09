62d736271be6d79f1cc2dfbe29bfb8e34ac6b1b9
Paulo Hecht <thehecht@gmail.com>
2016-07-09 11:57:46 -0300

    changed to question

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-import Task from './Task.jsx';
+import Question from './Question.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
+  getQuestions() {
     return [
       { _id: 1, text: 'This is task 1' },
       { _id: 2, text: 'This is task 2' },
@@ -12,20 +12,33 @@ export default class App extends Component {
     ];
   }
 
-  renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
+  renderQuestions() {
+    return this.getQuestions().map((question) => (
+      <Question key={question._id} question={question} />
     ));
   }
 
   render() {
     return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">Fórum Open Sanca</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            </div>
+          </div>
+        </nav>
         <ul>
-          {this.renderTasks()}
+          {this.renderQuestions()}
         </ul>
       </div>
     );
--- imports/ui/Question.jsx
+++ imports/ui/Question.jsx
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
 // Task component - represents a single todo item
-export default class Task extends Component {
+export default class Question extends Component {
   render() {
     return (
-      <li>{this.props.task.text}</li>
+      <li>{this.props.question.text}</li>
     );
   }
 }
 
-Task.propTypes = {
+Question.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  task: PropTypes.object.isRequired,
+  question: PropTypes.object.isRequired,
 };
